19a1322951964da7bd2ebe1499791f7efad1d690
shiro-han <shan99business@gmail.com>
2020-08-03 20:13:05 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,16 @@
-// Code DigitalClicker Component Here
+import React from 'react';
+
+export default class DigitalClicker extends React.Component {
+    constructor(props) {
+        super()
+        this.state = {timesClicked: 0}
+    }
+
+    clickFn = () => {
+        this.setState({timesClicked: ++this.state.timesClicked})
+    }
+
+    render() {
+        return <button onClick={this.clickFn}>{this.state.timesClicked}</button>
+    }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,44 @@
-// Code YouTubeDebugger Component Here
+import React from 'react';
+
+export default class YouTubeDebugger extends React.Component {
+    constructor(props) {
+        super()
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+                bitrate: 8,
+                video: {
+                    resolution: '1080p'
+                }
+            }
+        }
+    }
+
+    bitrateFn = () => {
+        this.setState({
+            settings: {
+                bitrate: 12,
+                video: {...this.state.settings.video}
+            }
+        })
+    }
+
+    resolutionFn = () => {
+        this.setState({
+            settings: {
+                bitrate: this.state.settings.bitrate,
+                video: {resolution: '720p'}
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.bitrateFn} className='bitrate'>bitrate</button>
+                <button onClick={this.resolutionFn} className='resolution'>resoultion</button>
+            </div>
+        )
+    }
+}
